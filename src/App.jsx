f7d9bce2dd247dfe31c7872f192c7551ac892ff4
sivarajagopalans <sivarajagopalans@gmail.com>
2024-07-22 19:19:22 +0530

    create blogform component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { BlogForm } from './components/BlogForm'
 
 function App() {
 
   return (
     <>
-      
+      <BlogForm />
     </>
   )
 }
---
+++ src/components/BlogForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BlogForm = () => {
+  return (
+    <div>
+        <h1>Blog Plartform</h1>
+    </div>
+  )
+}
