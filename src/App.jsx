bde08ca726505f3d6194d07c8036dbee646e8b1c
sivarajagopalans <sivarajagopalans@gmail.com>
2024-07-22 19:46:43 +0530

    create blogform component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BlogForm } from './components/BlogForm'
 
 function App() {
--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
 
 export const BlogForm = () => {
   return (
     <div>
-        <h1>Blog Plartform</h1>
+      <h1 className='text-center my-5'>Blog Plartform</h1>
+      <Container>
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label className='h4'>Title</Form.Label>
+                <Form.Control type="text" placeholder="Enter Blog Title" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label className='h4'>Content</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+              <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label>Default file input example</Form.Label>
+                <Form.Control type="file" />
+              </Form.Group>
+              <Button variant="secondary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
