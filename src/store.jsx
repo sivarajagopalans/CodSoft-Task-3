4fabac37598ea546f77913841640923b38c867b1
sivarajagopalans <sivarajagopalans@gmail.com>
2024-07-23 19:47:26 +0530

    config store

--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
+import blogReducer from './slices/blogSlices'
 export const store = configureStore({
     reducer: {
-
+        blogs: blogReducer
     }
 })
