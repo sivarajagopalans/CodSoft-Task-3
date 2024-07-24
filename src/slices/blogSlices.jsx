43f7b1d4b5e552ecc80b6d8738b4dfaea9c29445
sivarajagopalans <sivarajagopalans@gmail.com>
2024-07-24 10:28:25 +0530

    setup blogslices

--- src/slices/blogSlices.jsx
+++ src/slices/blogSlices.jsx
@@ -13,10 +13,21 @@ const blogSlice = createSlice({
             const id = Math.random() * 100;
             let blog = { ...action.payload, id };
             state.blogList.push(blog);
+        },
+        removeBlog: (state, action) => {
+            state.blogList = state.blogList.filter((blog) => blog.id != action.payload);
+        },
+        updateBlog: (state, action) => {
+            state.blogList = state.blogList.map((blog) => {
+                return blog.id === action.payload.id ? action.payload : blog;
+            })
+        },
+        setSeletedTBlog: (state, action) => {
+            state.selectedBlog = action.payload;
         }
     }
 })
 
 
-export const { addBlog } = blogSlice.actions;
+export const { addBlog, removeBlog, updateBlog, setSeletedTBlog } = blogSlice.actions;
 export default blogSlice.reducer;
