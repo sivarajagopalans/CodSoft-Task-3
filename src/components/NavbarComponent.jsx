cd74d5f9904cfb2c195162351603cf53e2ea61fa
sivarajagopalans <sivarajagopalans@gmail.com>
2024-07-24 10:44:50 +0530

    setup router

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 export const NavbarComponent = () => {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand>Blog Platform</Navbar.Brand>
+                    <Navbar.Brand><Link to='/' className='text-decoration-none h3'>Blog Platform</Link></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
                         <Nav className='d-flex w-25 justify-content-around'>
-                            <Nav.Link href="#deets">Home</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                               All Posts
-                            </Nav.Link>
+                            <Link to='/' className='h5 text-decoration-none'>Home</Link>
+                            <Link to='/all-posts' className='h5 text-decoration-none'>
+                                All Posts
+                            </Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
