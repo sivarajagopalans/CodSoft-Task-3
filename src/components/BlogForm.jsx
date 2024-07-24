82a14d0aff6484e9d4759665a7fab61b3a9d5d3c
sivarajagopalans <sivarajagopalans@gmail.com>
2024-07-24 11:17:45 +0530

    setup formsubmit

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import { Posts } from './Posts';
 import { useDispatch } from 'react-redux';
 import { addBlog } from '../slices/blogSlices';
 
@@ -15,11 +14,15 @@ export const BlogForm = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [image, setImage] = useState('');
+  
   const dispatch = useDispatch();
 
   const formSumbmit = (e) => {
     e.preventDefault();
     dispatch(addBlog({ title, content, image }));
+    setTitle('');
+    setContent('');
+    setImage('');
   };
 
   return (
@@ -30,16 +33,16 @@ export const BlogForm = () => {
             <Form>
               <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                 <Form.Label className='h4'>Title</Form.Label>
-                <Form.Control type="text" onChange={(e) => setTitle(e.target.value)} placeholder="Enter Blog Title" />
+                <Form.Control type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Enter Blog Title" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label className='h4'>Content</Form.Label>
-                <Form.Control as="textarea" onChange={(e) => setContent(e.target.value)}
+                <Form.Control as="textarea" value={content} onChange={(e) => setContent(e.target.value)}
                   rows={3} />
               </Form.Group>
               <Form.Group controlId="formFile" className="mb-3">
-                <Form.Label>Default file input example</Form.Label>
-                <Form.Control type="file" onChange={(e) => setImage(URL.createObjectURL(e.target.files[0]))} />
+                <Form.Label className='h4'>Image</Form.Label>
+                <Form.Control type="file"  onChange={(e) => setImage(URL.createObjectURL(e.target.files[0]))} />
               </Form.Group>
               <Button variant="secondary" type="submit" onClick={(e) => formSumbmit(e)}>
                 Submit
@@ -49,19 +52,6 @@ export const BlogForm = () => {
           </Col>
         </Row>
       </Container>
-      {/* {
-        image && image.map((value, index) => {
-          return <img src={value} alt="dfdf" />
-        })
-      } */}
-      {/* <h1>{title}</h1> */}
-
-      {/* {
-
-        (blogList.length !== 1) ? (blogList.map((value, index) => {
-          <Posts key={index} title={value.title} content={value.content} image={value.image} />
-        })) : <p>dfdk</p>
-      } */}
     </div>
   )
 }
