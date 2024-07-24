59c6667832d155816230fdaa81b2c585b9d81032
sivarajagopalans <sivarajagopalans@gmail.com>
2024-07-24 10:45:36 +0530

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BlogForm } from './components/BlogForm'
 import { NavbarComponent } from './components/NavbarComponent';
+import { Route, Routes } from 'react-router-dom';
+import { Posts } from './components/Posts';
 
 function App() {
 
   return (
     <>
       <NavbarComponent />
-      <BlogForm />
+      <Routes>
+        <Route index path='/' element={<BlogForm />} />
+        <Route path='/all-posts' element={<Posts />} />
+      </Routes>
     </>
   )
 }
